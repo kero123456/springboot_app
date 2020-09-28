d5fdca04ff2a166024fe46249e5264484f0787c0
jack <jack@123.com>
2020-09-28 16:52:56 +0800

    aaaaaa

--- src/main/java/com/qa/springboot/controller/UserController.java
+++ src/main/java/com/qa/springboot/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+	
 	private String ccc;
 	
 	@RequestMapping("/u")
